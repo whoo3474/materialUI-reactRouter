06ad553907c2718599a48f2d68f94f5ff4f6af69
JeongMin Woo <minhan_a@naver.com>
2019-01-29 23:31:50 +0900

    final

--- material-react-router/src/Components/App.js
+++ material-react-router/src/Components/App.js
@@ -19,7 +19,15 @@ class App extends Component {
       <BrowserRouter>
           <Layout writers={writers}>
             <Switch>
-              <Route exact path="/" render={() => <div>Home</div>}/>
+              <Route exact path="/" render={() =>
+                <div>
+                  {`이 사이트는 material-ui와 react-router을 이용하여 만든 사이트입니다.
+                  json-server 로 json 파일을 서버에 올리고, fetch로 받아서 값을 뿌려줍니다.
+                  npm start후 json-server --watch store.json --port 3004로
+                  3004포트에 json서버를 열어주면 됩니다.
+                  `}
+                </div>
+            }/>
               <Route path="/writers" render={
                 props=> <Writers {...props} writers={writers}/>
               }/>
--- material-react-router/src/Components/Writers/index.js
+++ material-react-router/src/Components/Writers/index.js
@@ -6,15 +6,7 @@ import {NotFound} from '../Errors';
 const index = ({ match:{url},writers}) => {
     return (
         <Fragment>
-            <ul>
-                {writers.map(({id, name}) => (
-                    <li key={id}>
-                        <Link to={`${url}/${id}`}>{name}</Link>
-                    </li>
-                ))}
-            </ul>
 
-            <Route path={url} render={()=> <h1> 작가를 선택해주세요</h1>}/>
             <Route path={`${url}/:writerId`} 
             render={(props)=> {
                 const writer = writers.find(writer=> writer.id === props.match.params.writerId)
